adfcd940998ca7c555c61fa0b046e36111d1ad5e
raahimkhan <muhammadr915@gmail.com>
2025-08-27 21:24:12 +0500

    feat: added component to take transcript meta data input and fetch it before playing audio

---
+++ src/screens/TranscriptAndAudioLoader.tsx
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import { View, TextInput, Platform, Alert, StyleSheet } from 'react-native';
+import { wp, hp } from '@raahimkhan23/react-native-responsive-utils';
+import { Button } from '@rneui/themed';
+import { fetchTranscriptMetaData } from '@utils/fetch-transcript-metadata';
+import { getInterleavedPhrases } from '@utils/get-interleaved-phrases';
+import { useGlobalStore } from '@global-store/global-store';
+import { useRouter } from 'expo-router';
+
+const TranscriptAndAudioLoader: React.FC = React.memo(() => {
+	const router = useRouter();
+
+	const updateGlobalState = useGlobalStore.getState().updateGlobalState;
+
+	const [transcriptURL, setTranscriptURL] = useState<string>(''); // meta data url
+	const [loading, setLoading] = useState<boolean>(false); // for button
+	const [disabled, setDisabled] = useState<boolean>(false); // for button
+
+	const handleSubmit = React.useCallback(async () => {
+		try {
+			// if no transcript URL provided, exit the function
+			if (!transcriptURL) {
+				Alert.alert('Error', 'Transcript URL is required!');
+				return;
+			}
+			setLoading(true);
+			setDisabled(true);
+			// fetch the transcript meta data
+			const transcriptMetaData =
+				await fetchTranscriptMetaData(transcriptURL);
+			// interleave the phrases based on transcript meta data
+			const interleavedPhrases = getInterleavedPhrases(
+				transcriptMetaData.speakers,
+				transcriptMetaData.pause
+			);
+			// update global state
+			updateGlobalState({
+				transcriptMetaData: transcriptMetaData,
+				interleavedPhrases: interleavedPhrases,
+			});
+			setLoading(false);
+			setDisabled(false);
+			// navigate to transcript player
+			router.push('/transcript-player');
+		} catch (error) {
+			setLoading(false);
+			setDisabled(false);
+			const e = error as { statusCode?: number; message: string };
+			if (e.statusCode === 400) {
+				Alert.alert('Error', e.message);
+			} else {
+				Alert.alert(
+					'Error',
+					'Something went wrong while fetching transcript meta data!'
+				);
+			}
+		}
+	}, [transcriptURL]);
+
+	return (
+		<View style={styles.container}>
+			{/* input for transcript meta data url */}
+			<TextInput
+				value={transcriptURL}
+				onChangeText={setTranscriptURL}
+				placeholder="Enter transcript meta data URL..."
+				placeholderTextColor={'gray'}
+				style={styles.textInput}
+				autoCapitalize="none"
+				keyboardType="url"
+			/>
+			{/* submit button */}
+			<Button
+				title="Let's go..!"
+				loading={loading}
+				disabled={disabled}
+				loadingProps={{
+					size: Platform.OS === 'web' ? 'large' : 'small',
+					color: 'black',
+				}}
+				titleStyle={styles.titleStyle}
+				buttonStyle={styles.buttonStyle}
+				containerStyle={styles.containerStyle}
+				onPress={handleSubmit}
+			/>
+		</View>
+	);
+});
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#F6F6F9',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	textInput: {
+		width: wp(80),
+		height: Platform.OS === 'web' ? hp(12) : hp(6),
+		borderWidth: 1,
+		borderColor: 'black',
+		borderRadius: 7,
+		backgroundColor: 'white',
+		paddingLeft: wp(3),
+		color: '#1B1B1B',
+		fontFamily: 'OutfitMedium',
+		fontSize: wp(3.5),
+	},
+	titleStyle: {
+		fontFamily: 'OutfitSemiBold',
+		fontSize: wp(3.5),
+	},
+	buttonStyle: {
+		backgroundColor: 'black',
+		borderWidth: 0,
+		borderRadius: 10,
+		width: wp(80),
+	},
+	containerStyle: {
+		marginTop: hp(5),
+	},
+});
+
+export default TranscriptAndAudioLoader;
