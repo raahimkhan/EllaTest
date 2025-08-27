11ec23351321c5273877640b2be4d8f66c1531e3
raahimkhan <muhammadr915@gmail.com>
2025-08-27 14:19:36 +0500

    feat: incorporated logic to play and pause the loaded audio along with error handling

--- src/utils/toggle-play-pause.ts
+++ src/utils/toggle-play-pause.ts
@@ -1,8 +1,24 @@
 import { useGlobalStore } from '@global-store/global-store';
+import { AudioPlayer } from 'expo-audio';
+import { Alert } from 'react-native';
 
-// this function will be used to toggle music player on/off and handle related logic
-export const togglePlayPause = () => {
-	const isPlaying = useGlobalStore.getState().isPlaying;
-	const updateGlobalState = useGlobalStore.getState().updateGlobalState;
-	updateGlobalState({ isPlaying: !isPlaying });
+// this function will be used to toggle music player pause/resume and handle related logic
+export const togglePlayPause = (player: AudioPlayer) => {
+	try {
+		if (player.isLoaded) {
+			const updateGlobalState =
+				useGlobalStore.getState().updateGlobalState;
+			const isPlaying = useGlobalStore.getState().isPlaying;
+			if (player.playing) {
+				player.pause();
+			} else {
+				player.play();
+			}
+			updateGlobalState({ isPlaying: !isPlaying });
+		} else {
+			Alert.alert('Error', 'Player is not loaded!');
+		}
+	} catch {
+		Alert.alert('Error', 'Something went wrong! Cannot play audio.');
+	}
 };
